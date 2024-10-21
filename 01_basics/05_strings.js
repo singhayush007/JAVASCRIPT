33b71f824218ec4a870f0e9bdbfedb2a0a955f0c
Ayush singh <80886698+singhayush007@users.noreply.github.com>
2024-10-21 10:25:11 +0000

    about strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,35 @@
+const name  = "Ayush"
+const repoCount = 50
+
+// console.log(name + repoCount + "value");
+
+console.log(`Hello my name is ${name} and my repo Count is $ {repoCount}`);
+
+const gameName = new String ('Ayushhh- sh')
+
+
+console.log(gameNmae[0]);
+console.log(gameNmae.__proto__);
+
+console.log(gameNmae.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+
+
+const newString = gameName.substring( 0 , 4);
+console.log(newString);
+const anotherString = gameName.slice(-8 , 4)
+console.log(anotherString); 
+
+
+const newStringOne = "   Ayush  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+const url = "https://ayush.com/ayush  singh";
+
+console.log(url.replace('%20' , '-'));
+console.log(url.includes('Ayush'))
+
+console.log(gameName.split('-'));
