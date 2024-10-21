dea8f6592d33ddc413087a765414f2adb4635f99
Ayush singh <80886698+singhayush007@users.noreply.github.com>
2024-10-21 11:59:23 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,31 @@
+const score = 400
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(1));
+
+const otherNumber = 23.8966
+//console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString(`en-IN`));
+
+/// +++++++++++++++++ Maths ++++++++++++++++++ //
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round.ceil(4.3));
+console.log(Math.flor(4.9));
+console.log(Math.min(4 , 3 , 5 ,6));
+console.log(Math.max(3 , 4 , 5 ,6 ));
+console.log((Math.random()*10) + 1);
+
+const mun = 10
+const max = 20
+
+console.log(math.floor(math.random() * (max - min + 1)) + min);
+
+
+
