a7b5d01afb050614332ad0652168aaba423ad881
Ayush singh <80886698+singhayush007@users.noreply.github.com>
2024-10-23 10:17:38 +0000

    scopes in js

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,63 @@
+//var c = 300
+
+//let a = 10
+//const b = 20
+//var c = 30
+
+let. a = 100
+
+if(true){
+    let a = 10
+    const b = 20
+    console.log("INNER: " , a)
+}
+for (let i = 0; i< Array.length; i++){
+    const element = array[i];
+}
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+
+function one(){
+    const username = "Ayush"
+
+    function two (){
+        const website = "Youtube"
+        //console.log(username);
+    }
+    //console.log(website);
+
+    //two()
+}
+
+//one()
+
+if(true){
+    const username = "Ayush"
+    if(username === "Ayush")
+{
+    const website = "youtube"
+    //console.log(username + website );
+}
+
+    //console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++ INTERESTING ++++++++++ //
+
+console.log(addone(5))
+   return num + 1
+
+function addone(num){
+    return num + 1
+}
+addone(5)
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
