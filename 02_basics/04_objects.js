12535bc93746322a280a6ff242700b6066830a4b
Ayush singh <80886698+singhayush007@users.noreply.github.com>
2024-10-23 08:46:51 +0000

    json talk and object destructuring

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,80 @@
+//const tinderUser = new Object()
+const tinderUser ={}
+
+tinder.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+
+
+//console.log(tinderUser);
+
+const regularuser = {
+    email: "some@gmail.com",
+    fullname = {
+        userfullname: {
+            firstname: "Ayush",
+            lastname : "Singh"
+        }
+    }
+}
+//console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a" , 2: "b"}
+const obj2 = {3: "c" , 4: "d"}
+const obj4= {5: "a" , 6: "b"}
+
+//const obj3 = {obj 1 , obj2}
+//const obj3 = object.assign({} , obj1 , obj2 , obj4)
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+//const users= [
+    {
+        {
+            {
+                {
+
+                }
+            }
+        }
+    }
+// ]
+
+use[1].emailconsole.log(tinderUser);
+
+//console.log(object.keys(tinderUser));
+//console.log(object.value(tinderUser));
+//console.log(object.entries(tinderUser));
+
+//console.log(tinderUser.hasOwnProperty('isLogged'))
+
+
+const course = {
+    coursename : "js in hindi",
+    price : "999",
+    courseInstructor: "Ayush"
+
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+//console.log(instructor);
+
+
+//{
+//   name: "Ayush",
+//  "coursename : "Js in hindi",
+//   "price" : "free"
+//}
+
+[
+  {},
+  {},
+  {}
+]
+
+
