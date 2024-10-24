1eb3997898876ee45de1a44e7d5db266ceb6b293
Ayush singh <80886698+singhayush007@users.noreply.github.com>
2024-10-24 03:54:22 +0000

    switch in js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,22 @@
+// switch 
+
+
+const month = "march" 
+
+switch (month) {
+    case "jan":
+        consolelog("Jan");
+        break;
+     case "feb" :
+        console.log("feb");
+        break;
+        case "march":
+            console.log("March");
+            break;
+            case "april":
+            console.log("april");
+            break;
+    default:
+        console.log("default case match");
+        break;
+}
