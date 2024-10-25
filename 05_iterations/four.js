ab1db73757309e5d04a197a97cfb1dfd18120960
Ayush singh <80886698+singhayush007@users.noreply.github.com>
2024-10-25 00:49:25 +0000

    for in for of

---
+++ 05_iterations/four.js
@@ -0,0 +1,33 @@
+const myObject = {
+    js : 'javascript' , 
+    cpp : 'c++',
+    rb : "ruby",
+    swift : "swift by apple" 
+}
+
+for (const key in myObject){
+    //console.log(myObject[key]);
+}
+
+for (const key in myObject){
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+for(const key in ["js", "rb", "py", "java", "cpp"]) {
+    //console.log(key);
+}
+
+const programming = ["js" , "rb" , "py" , "java" , "cpp"]
+for(const key in programming ["js", "rb", "py", "java", "cpp"][key]) {
+    console.log(key);
+}
+
+const map = new map ()
+map.set('IN' , "India")
+map.set('USA' , "United States of America" )
+map.set('UK' , "Unkted Kingdom")
+
+for (const key in map){
+    //console.log(key);
+}
+
